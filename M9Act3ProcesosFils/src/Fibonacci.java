b2cc8eb741428e0c528c2e77a8444a189887e73e
Alumne <Alumne@104-PC04>
2019-10-29 17:29:17 +0100

    temps millorat, resultat final millorat

--- M9Act3ProcesosFils/src/Fibonacci.java
+++ M9Act3ProcesosFils/src/Fibonacci.java
@@ -6,40 +6,27 @@ import java.util.GregorianCalendar;
 public class Fibonacci {
 	
 	public static long calculaFibonacci(long numero) {
+				
 		double calcul = java.lang.Math.cos(54879854);
+		
 		if (numero==0) { return 0; }
 		else if (numero==1) { return 1; }
-		else { 
-			return (calculaFibonacci(numero-2) + calculaFibonacci(numero-1)); 
+		else {
+			return (calculaFibonacci(numero-2) + calculaFibonacci(numero-1));
 		}
-	}
-	
-	public static void tempsInici() {
-		//Mostra el resultat
-		Calendar calendario = new GregorianCalendar();
-		System.out.println("Hora inici tasca: "
-				+ calendario.get(Calendar.HOUR_OF_DAY) + ":"
-				+ calendario.get(Calendar.MINUTE) + ":"
-				+ calendario.get(Calendar.SECOND));
-		
 		
 	}
-	
-	public static void tempsFi() {
-		//Mostra el resultat
-		Calendar calendario = new GregorianCalendar();
-		System.out.println("Hora fi tasca: "
-				+ calendario.get(Calendar.HOUR_OF_DAY) + ":"
-				+ calendario.get(Calendar.MINUTE) + ":"
-				+ calendario.get(Calendar.SECOND));
-			
-	}
-	
 
-	public static void main(String[] args) {
-		tempsInici();
-		System.out.println(calculaFibonacci(35));
-		tempsFi();
+	public static void main(String[] args) throws InterruptedException {
+		long inicio = System.currentTimeMillis();
+		
+		int NumeroFibonacci=34;
+				
+		System.out.println(calculaFibonacci(NumeroFibonacci));
+		long fin = System.currentTimeMillis();
+		double temps = (double) ((fin - inicio)/1000);
+	         
+	    System.out.println("Tarda: " + temps +" segons a calcular Fibonacci de " + NumeroFibonacci );
 		
 				
 	}
