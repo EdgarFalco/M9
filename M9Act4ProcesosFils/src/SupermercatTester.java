4c374e732085f9b65cb61af29245524b4e92caf5
Edgar <Edgar@192.168.1.128>
2019-11-03 20:47:54 +0100

    client

--- M9Act4ProcesosFils/src/SupermercatTester.java
+++ M9Act4ProcesosFils/src/SupermercatTester.java
@@ -9,14 +9,10 @@ public class SupermercatTester {
     public static void main(String[] args) {
 
         ArrayList<Client>clients = new ArrayList<Client>();
-        clients.add(new Client("Client 1", new int[] { 2, 2, 1, 5, 2 })); 
-        clients.add(new Client("Client 2", new int[] { 1, 1, 5, 1, 1 })); 
-        clients.add(new Client("Client 3", new int[] { 5, 3, 1, 5, 2 })); 
-        clients.add(new Client("Client 4", new int[] { 2, 4, 3, 2, 5 })); 
-        clients.add(new Client("Client 5", new int[] { 1, 3, 2, 2, 3 })); 
-        clients.add(new Client("Client 6", new int[] { 4, 2, 1, 3, 1 })); 
-        clients.add(new Client("Client 7", new int[] { 3, 3, 2, 4, 7 })); 
-        clients.add(new Client("Client 8", new int[] { 6, 1, 3, 1, 3 })); 
+        for (int i = 0; i < 2; i++) {
+			clients.add(new Client("Client "+ i, new int[]{ 2, 2, 1, 5, 2 }));
+		}
+        
         
         
         long init = System.currentTimeMillis();  // Inici
