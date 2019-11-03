94398b76d63c5eb9550c5e9b8288741628592a05
Edgar <Edgar@192.168.1.128>
2019-11-03 13:28:25 +0100

    +proves

--- M9Act4ProcesosFils/src/Bombolla.java
+++ M9Act4ProcesosFils/src/Bombolla.java
@@ -1,63 +1,38 @@
 
-import java.io.IOException;
-import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.RecursiveTask;
+import java.io.*;
  
-public class Bombolla extends RecursiveTask<Short>
+public class Bombolla
 {
-	private static final int LIMITAR = 10000;
-	short[] array;
-	private int inici, fi;
-	
-	public Bombolla(short[] data, int inici, int fi) {
-		this.array = data;
-		this.inici = inici;
-		this.fi = fi;
-	}
+    public static void main(String arg[]) throws IOException
+    {
+    	//Crea un array de mida 10 o la que especifiquem
+    	int[] data = createArray(10);
+    	//Ordena la llista i mostra el proces
+    	burbuja(data);
+    	//Mostra el resultat final de la ordenacio
+    	System.out.println("Ordenacio Completada: ");
+    	for(int i = 0;i < data.length; i++)
+        {
+            System.out.print(data[i]+" ");
+        }
+    }
+    
   //Metode, per crear un array on especifiques la mida
-  	private static short[] createArray(int size) {
-  		short[] ret = new short[size];
+  	private static int[] createArray(int size) {
+  		int[] ret = new int[size];
   		for (int i = 0; i < size; i++) {
-  			ret[i] = (short) (1000 * Math.random());
-  			if (i == ((short) (size * 0.9))) {
+  			ret[i] = (int) (1000 * Math.random());
+  			if (i == ((int) (size * 0.9))) {
   				ret[i] = 105;
   			}
   		}
   		
   		return ret;
   	}
-  	
-	private Short getArrays() {
-		Bombolla bombolla1;
-		Bombolla bombolla2;
-		
-		int mig = (inici + fi) / 2 + 1;
-		
-		bombolla1 = new Bombolla(array, inici, mig);
-		bombolla1.fork();
-		
-		bombolla2 = new Bombolla(array, mig, fi);
-		bombolla2.fork();
-		
-		return (short) Math.max(bombolla1.join(),bombolla2.join());
-	}
-	
-	@Override
-	protected Short compute() {
-	
-	if(fi - inici <= LIMITAR){
-				
-				return getArrays();
-			}else{
-				
-				return null;
-			}
-		
-
-	}
+    
         
-    //Metode, Bombolla
-    private static short[] burbuja(short arreglo[])
+    //Metode, Bombolla Ordena de mes gran a més petit una llista
+    private static void burbuja(int arreglo[])
     {
         for(int i = 0; i < arreglo.length - 1; i++)
         {        	         	
@@ -65,55 +40,21 @@ public class Bombolla extends RecursiveTask<Short>
             {
                 if (arreglo[j] < arreglo[j + 1])
                 {
-                    short tmp = arreglo[j+1];
+                    int tmp = arreglo[j+1];
                     arreglo[j+1] = arreglo[j];
                     arreglo[j] = tmp;
                 }
-                
-            }
-            //Mostra l'array quan l'algoritme esta ordenant
-            System.out.println("ordenant...");
+             
+                 
+             }
+            System.out.println("ordenant...");	
             for (int k = 0; k < arreglo.length; k++) {
-             	System.out.print(arreglo[k]+" ");
+           	 System.out.print(arreglo[k]+" ");
             }
             System.out.println("\n");
-            
         }
-        return arreglo;
+        
     }
-    
-    public static void main(String arg[]) throws IOException
-    {
-    	    	    	
-    	short[] data = createArray(10000);
-    	
-    	// Mira el número de processadors
-		System.out.println("Inici càlcul");
-		ForkJoinPool pool = new ForkJoinPool();
-		
-		int inici=1;
-		int fi= data.length;
-		
-		Bombolla tasca1 = new Bombolla(data, inici, fi);
-		
-		// crida la tasca i espera que es completin
-		int result1 = (int)pool.invoke(tasca1);
-		//Guarda la tasca que ha sigut treballada amb fils
-		int result= tasca1.join();
-		
-    	//Mostra el resultat final
-    	System.out.println("Final: ");
-    	
-    	for(int i = 0;i < data.length; i++)
-        {
-    		System.out.print(data[i]+" ");
-        }
-    	
-    	
-		System.out.println("\nCompletat");
-		System.out.println ("Màxim es " + result);
-    }
-    
-         
+     
 }
 
