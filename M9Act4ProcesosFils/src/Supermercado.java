6b0079c26e2706dcd6bc66dc4ccd985b19872f65
Edgar <Edgar@192.168.1.128>
2019-11-08 13:09:17 +0100

    final

--- M9Act4ProcesosFils/src/Supermercado.java
+++ M9Act4ProcesosFils/src/Supermercado.java
@@ -34,7 +34,7 @@ public class Supermercado implements Runnable {
 		System.out.println("Creando el " + cliente.getNombre() + " con " + cliente.getArrayArticulos().length + " articulos " + Arrays.toString(cliente.getArrayArticulos()));
 		System.out.println(cliente.getNombre() + " pasa por caja...");
 
-		//for (int i = 0; i < cliente.getArrayArticulos().length; i++) {
+		for (int i = 0; i < cliente.getArrayArticulos().length; i++) {
 
 			// Para iniciar cada articulo cada determinado tiempo, utilizamos la funcion "iniciarArticulos"
 			// Donde pasamos el numero que haya almacenado en el array de Articulos
@@ -44,7 +44,18 @@ public class Supermercado implements Runnable {
 				e.printStackTrace();
 			}
 
+			// Cuando estemos en la ultima posicion del array de los articulos, imprimimos que se ha finalizado
+			if (i == cliente.getArrayArticulos().length - 1) {
 
+				System.out.println(cliente.getNombre() + " articulo " + (i + 1) + "/" + cliente.getArrayArticulos().length +
+						" (" + cliente.getArrayArticulos()[i] + " segundos)" + "...FINALIZADO");
+			} else {
+
+				System.out.println(cliente.getNombre() + " articulo " + (i + 1) + "/" + cliente.getArrayArticulos().length +
+						" (" + cliente.getArrayArticulos()[i] + " segundos)");
+			}	
+		}
+	}
 
 	public static void main(String[] args) throws InterruptedException {
 
@@ -53,18 +64,27 @@ public class Supermercado implements Runnable {
 
 		for (int i = 0; i < 10; i++) {  
 
-			
+			// Array para almacenar la cantidad de articulos que tendra un cliente
+			int[] numArticulos = new int[(int)(Math.random() * 30 - 1) + 1];
+
+			// Rellenamos cada articulo con un numero aleatorio de 2 a 8 que seraan los segundos
+			for (int j = 0; j < numArticulos.length; j++) {
+				numArticulos[j] = (int)(Math.random() * 8 - 1) + 1;
+			}
+			// Añadimos el cliente creado con el nombre y el array de numero de articulos
+			// donde tendra el numero aleatorio para saber cuando se tiene que ejecutar el siguiente articulo
+			clientes.add(new Cliente("Cliente "+ (i + 1) , numArticulos));
 		}  
 
 		long init = System.currentTimeMillis();
 
-		
+		// Pasamos el numero de hilos que queremos ejecutar
 		ExecutorService executor = Executors.newFixedThreadPool(cajas);
 
-		
+		// Por cada cliente que pasamos, esperamos 3 segundos y ejecutamos los articulos
 		for (Cliente cliente: clientes) {
 
-			
+			// Para crear un cliente esperamos 3 segundos
 			Thread.sleep(3000);
 			Runnable caja = new Supermercado(cliente, init);
 			executor.execute(caja);
@@ -72,5 +92,3 @@ public class Supermercado implements Runnable {
 		executor.shutdown();
 	}
 }
-
-
