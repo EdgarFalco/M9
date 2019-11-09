cbedfe5ee0746719c1af1a9c74ac6b1b74d2f9a8
Edgar <Edgar@192.168.1.128>
2019-11-09 16:42:22 +0100

    prova 501

--- M9Act4ProcesosFils/src/BombollaFils.java
+++ M9Act4ProcesosFils/src/BombollaFils.java
@@ -3,18 +3,17 @@ public class BombollaFils implements Runnable {
 	private int[] array;
 	private int inicio;
 	private int finalArray;
-	
+	//Constructor
 	public BombollaFils (int[]array, int inicio, int finalArray){
 		
 		this.array = array;
 		this.inicio = inicio;
 		this.finalArray = finalArray;
 	}
-		
+	
+	//Ordena de mes gran a mes petit
 	public void bombollaOrdena(int[] array, int inicio, int finalArray) {
-		//int contador = 0;
-		//contador ++;
-		//System.out.println("Llista"+contador);
+		
 		for(int i = inicio; i < finalArray; i++)
         {        	         	
             for(int j = inicio; j < finalArray; j++)
@@ -26,19 +25,15 @@ public class BombollaFils implements Runnable {
                     array[j] = tmp;
                 }
             }
-//            System.out.println("ordenant...");	
-//            for (int k = inicio; k <= finalArray; k++) {
-//           	 System.out.print(array[k]+" ");
-            }
-            //System.out.println("\n");
+
+       }
     }
-    
 	
-	//}
-
 	public static void main(String[] args) {
-
+		//Creem array
 		int[] arrayNum = generarNumeros(12);
+		
+		//Controlem les posicions de l'array
 		int primerCuarto = 0;
 		int finalPrimerCuarto = (arrayNum.length / 4) - 1;
 
@@ -46,8 +41,12 @@ public class BombollaFils implements Runnable {
 		int finalSegundoCuarto = (arrayNum.length / 2) - 1;
 
 		int inicioTercerCuarto = finalSegundoCuarto + 1;
-		int finalTercerCuarto = arrayNum.length - 1;
+		int finalTercerCuarto = finalPrimerCuarto + finalSegundoCuarto +1;
 		
+		int inicioUltimo = finalTercerCuarto + 1;
+		int finalFinal = arrayNum.length - 1;
+		
+		//Mostra la llista creada
 		System.out.println("Creacio llista: ");
 		for (int i = 0; i < arrayNum.length; i++) {
 			System.out.print(arrayNum[i] + " ");
@@ -60,12 +59,12 @@ public class BombollaFils implements Runnable {
 		Runnable hilo2 = new BombollaFils(arrayNum, segundoCuarto, finalSegundoCuarto);
 		Thread thread2 = new Thread(hilo2);
 		
-		Runnable hilo3 = new BombollaFils(arrayNum, 6, 8);
+		Runnable hilo3 = new BombollaFils(arrayNum, inicioTercerCuarto, finalSegundoCuarto);
 		Thread thread3 = new Thread(hilo3);
 		
-		Runnable hilo4 = new BombollaFils(arrayNum, 9, 11);
+		Runnable hilo4 = new BombollaFils(arrayNum, inicioUltimo, finalFinal);
 		Thread thread4 = new Thread(hilo4);
-		
+		//Inicia fils
 		new Thread(thread1).start();
 		
 		new Thread(thread2).start();
@@ -74,7 +73,7 @@ public class BombollaFils implements Runnable {
 		
 		new Thread(thread4).start();
 				
-
+		//Mostra el resultat de les 4 llistes ordenades
 		System.out.println("Part 1 Llista ordenada: ");
 		for (int i = 0; i <= 2; i++) {
 			System.out.print(arrayNum[i] + " ");
@@ -100,11 +99,9 @@ public class BombollaFils implements Runnable {
 		for (int i = 9; i <= 11; i++) {
 			System.out.print(arrayNum[i] + " ");
 		}
-		
-		
 	}
 
-// Retorna un array con números aleatorios del 1 al 100 excluyéndolo
+// Retorna un array amb numeros aleatoris de 0 al 999
 	private static int[] generarNumeros(int size) {
 
 		int[] array = new int[size];
@@ -115,23 +112,8 @@ public class BombollaFils implements Runnable {
 		}
 		return array;
 	}
-
-//// Ordena un array que pasamos por parámetros de menor a mayor e imprime cada cambio que hace
-//	private static void burbuja(int arreglo[], int inici, int fi) {
-//
-//		for (int i = inici; i < fi - 1; i++) {
-//
-//			for (int j = 0; j < arreglo.length - 1; j++) {
-//
-//				if (arreglo[j] < arreglo[j + 1]) {
-//					int tmp = arreglo[j + 1];
-//					arreglo[j + 1] = arreglo[j];
-//					arreglo[j] = tmp;
-//				}
-//			}
-//		}
-//	}
-
+	
+	//Medore run, executa el fil
 	@Override
 	public void run() {
 		bombollaOrdena(this.array, this.inicio, this.finalArray);
