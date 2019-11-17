b652a076129aa51c21b31f076c52d6ee50b77abb
Edgar <Edgar@192.168.1.128>
2019-11-18 00:50:42 +0100

    eliminar naves

--- M9Act7ProcesosFils/src/NauEspaial.java
+++ M9Act7ProcesosFils/src/NauEspaial.java
@@ -1,6 +1,9 @@
 import java.awt.*;
 import java.util.*;
 import javax.swing.*;
+
+
+
 import java.awt.event.*;
 
 //CLASS NAU ESPAIAL ================================================================================================================================
@@ -45,8 +48,10 @@ public class NauEspaial extends javax.swing.JFrame {
 		NauEspaial f = new NauEspaial();
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f.setTitle("Naus Espaials");
-		f.setContentPane(new PanelNau());
-		f.setSize(480, 560);
+		final int altura = 480;
+		final int amplada = 560;
+		f.setContentPane(new PanelNau(altura, amplada));
+		f.setSize(altura, amplada);
 		f.setVisible(true);
 	}
 }
@@ -55,16 +60,22 @@ public class NauEspaial extends javax.swing.JFrame {
 
 class PanelNau extends JPanel implements Runnable, KeyListener {
 	private int numNaus = 10;
-	Nau[] nau;
+	Vector<Nau> nau = new Vector<Nau>();
 	Nau nauPropia;
 	Bala bala;
 	Bala balaEnemic;
 	Vector<Bala> listBalas = new Vector<Bala>();
+	int altura;
+	int amplada;
 	
-	public PanelNau() {
+	//Afegim al Panell la altura i la amplada per poder controlar quan fer desapareixer les bales
+	public PanelNau(int altura, int amplada) {
 		setBackground(Color.GRAY);
-		nau = new Nau[numNaus];
-		for (int i = 0; i < nau.length; i++) {
+		
+		this.altura = altura;
+		this.amplada = amplada;
+		
+		for (int i = 0; i < nau.capacity(); i++) {
 			Random rand = new Random();
 			int velocitat = (rand.nextInt(3) + 5) * 10;
 			int posX = rand.nextInt(100) + 30;
@@ -72,53 +83,82 @@ class PanelNau extends JPanel implements Runnable, KeyListener {
 			int dX = rand.nextInt(3) + 1;
 			int dY = rand.nextInt(3) + 1;
 			String nomNau = Integer.toString(i);
-			nau[i] = new Nau(nomNau, posX, posY, dX, dY, velocitat, "/images/nau3.png");
+			nau.add(new Nau(nomNau, posX, posY, dX, dY, velocitat, altura, amplada, "/images/nau3.png"));
 			
 		}
 		
-		// Creo la nau propia
-		nauPropia = new Nau("NauNostra", 200, 400, 10, 0, 100, "/images/nau2.png" );
-		//Crea la bala
-		bala = new Bala();
-		balaEnemic = new Bala();
+		addKeyListener(this);
+		setFocusable(true);
 		
-
+		// Creo la nau propia
+		nauPropia = new Nau("NauNostra", 200, 400, 10, 0, 100, altura, amplada, "/images/nau2.png" );
+	
 		// Creo fil per anar pintant cada 0,1 segons el joc per pantalla
 		Thread n = new Thread(this);
 		n.start();
-
-		// Creo listeners per a que el fil principal del programa gestioni
-		// esdeveniments del teclat
-		addKeyListener(this);
-		setFocusable(true);
 
 	}
 
 	public void run() {
 		System.out.println("Inici fil repintar");
-		while (true) {
-			try {
-				Thread.sleep(100);
-			} catch (Exception e) {
-			} // espero 0,1 segons
-			// System.out.println("Repintant");
-			repaint();
-		}
+		while(true) {
+			try { Thread.sleep(10);} catch(Exception e) {} // espero 0,01 segons
+			
+			if(numNaus != 0){
+				repaint();
+				try {
+					
+					comprobarColisiones();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				
+				destruirNaus();
+			}else{
+				
+				for (int i = 0; i < nau.size(); i++) {
+					nau.elementAt(i).interrupt();
+				}
+				nauPropia.interrupt();
+				
+			}
+		}                   
 	}
-
+	
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		//Pinta les naus enemigues
-		for (int i = 0; i < nau.length; ++i){
-			nau[i].pinta(g);
+		for(int i=0; i<nau.size();++i) {
+			nau.elementAt(i).pinta(g);
+			nauPropia.pinta(g);
 		}
-		//Pinta la nostra nau
-		nauPropia.pinta(g);
 		
-		//Pinta els dispars de la nostra nau
-		for (int i = 0; i < listBalas.size(); i++) {
-			listBalas.elementAt(i).pinta(g);
+			for (int j = 0; j < listBalas.size(); j++) {
+				listBalas.elementAt(j).pinta(g);
+			}
+			
+	}
+	
+	//Borrem la nau del vector 
+	public void destruirNaus(){
+		for (int j = 0; j < nau.size(); j++) {
+			if(nau.elementAt(j).isDestruit()){
+				
+				nau.elementAt(j).interrupt();
+				nau.elementAt(j).setPass(false);
+				nau.remove(j);
+				System.out.println("Borra nau");
+				
+			}
 		}
+	} 
+	
+	public void comprobarColisiones() throws InterruptedException{
+		
+		Choque colisio = new Choque(2);
+		Thread colisioBala = new Thread(colisio);
+		colisioBala.start();
+		
+
 	}
 
 	// Metodes necesaris per gestionar esdeveniments del teclat
@@ -137,32 +177,114 @@ class PanelNau extends JPanel implements Runnable, KeyListener {
 			nauPropia.dreta();
 		} // System.out.println("a la dreta"); }
 		if (e.getKeyCode() == 32) {
-			bala = new Bala(nauPropia.getX(), nauPropia.getY(), 40, "/images/bala1.png");
+			bala = new Bala(60, altura, amplada, nauPropia.getX(), nauPropia.getY(), false, "/images/bala1.png");
 			listBalas.add(bala); //tecla espai, dispar normal
-		} // System.out.println("a la dreta"); }
-		if (e.getKeyCode() == 71 ) { //tecla G, dispar gran
-			bala = new Bala(nauPropia.getX(), nauPropia.getY(), 40, "/images/bala2.png");
-			listBalas.add(bala);
+			bala.start();
 		}
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
+		
 	}
+//CLASS CHOQUE ===================================================================	
+	private class Choque implements Runnable{
+		
+		private int comproba;
+		private boolean pass;
+		
+		public Choque(int comproba){
+			this.comproba= comproba;
+		}
+
+		public void run() {
+			pass = true;
+			
+			while(pass){
+				if(comproba == 1){
+					colisioEnemics();
+				}else if(comproba==2){
+					try {
+						eliminaNausEnemigues();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}else {
+					try {
+						colisioBales();
+					} catch (InterruptedException e) {
+						e.printStackTrace(); 
+					}
+				}
+			
+			pass=false;
+			}
+		}
+
+		public void eliminaNausEnemigues() throws InterruptedException{
+			for (Bala m : listBalas){
+				Rectangle rectangle = m.getBounds();
+				for (Nau nauEnemiga : nau) {
+					Rectangle rectangle2 = nauEnemiga.getBounds();
+					if (rectangle.intersects(rectangle2)) {
+						nauEnemiga.setDsx(0);
+						nauEnemiga.setDsy(0);
+						nauEnemiga.setDestruit(true);
+						m.colisio(getGraphics());
+						numNaus--;
+					}
+				}
+			}
+		}
+
+		public void colisioEnemics(){
+			Rectangle rectangle3 = nauPropia.getBounds();
+			for (Nau nauEnemiga : nau) {
+				Rectangle rectangle2 = nauEnemiga.getBounds();
+				if (rectangle3.intersects(rectangle2)) {
+					nauEnemiga.setDsx(0);
+					nauEnemiga.setDsy(0);
+					nauEnemiga.interrupt();
+					
+				}
+			}
+		}
+
+		public void colisioBales() throws InterruptedException{
+			for (int i = 0; i < nau.size(); i++) {
+				for (Bala bala1 : nau.elementAt(i).listBalas) {
+						Rectangle rectangle5 = bala1.getBounds();
+						Rectangle rectangle4 = nauPropia.getBounds();
+						
+						if (rectangle5.intersects(rectangle4)) {
+							bala1.colisio(getGraphics());
+						}   
+					}
+				}
+			}
+		}
+
+	
 }
 
 //CLASS NAU ================================================================================================================================
 
 class Nau extends Thread {
+
 	private String nomNau;
 	private int x, y;
 	private int dsx, dsy, v;
 	private int tx = 10;
 	private int ty = 10;
-	private String img;
+	private String img = "/images/nau3.png";
 	private Image image;
-		
-	public Nau(String nomNau, int x, int y, int dsx, int dsy, int v, String direccioImatge) {
+	private int altura;
+	private int amplada;
+	private boolean destruit;
+	private boolean pass;
+	Vector<Bala> listBalas = new Vector<Bala>();
+
+	public Nau(String nomNau, int x, int y, int dsx, int dsy, int v, int altura, int amplada, String direccioImatge) {
 		this.nomNau = nomNau;
 		this.x = x;
 		this.y = y;
@@ -171,7 +293,12 @@ class Nau extends Thread {
 		this.v = v;
 		this.img = direccioImatge;
 		image = new ImageIcon(Nau.class.getResource(direccioImatge)).getImage();
+		this.altura = altura;
+		this.amplada = amplada;
+		tx = image.getHeight(null);
+		ty = image.getWidth(null);
 		Thread t = new Thread(this);
+		destruit = false;
 		t.start();
 	}
 	
@@ -213,11 +340,18 @@ class Nau extends Thread {
 		this.dsx = 10;
 	}
 	//Getters and setters
+	public String getNomNau() {
+		return nomNau;
+	}
+
+	public void setNomNau(String nomNau) {
+		this.nomNau = nomNau;
+	}
+
 	public int getX() {
 		return x;
 	}
 
-	
 	public void setX(int x) {
 		this.x = x;
 	}
@@ -229,55 +363,268 @@ class Nau extends Thread {
 	public void setY(int y) {
 		this.y = y;
 	}
+
+	public int getDsx() {
+		return dsx;
+	}
+
+	public void setDsx(int dsx) {
+		this.dsx = dsx;
+	}
+
+	public int getDsy() {
+		return dsy;
+	}
+
+	public void setDsy(int dsy) {
+		this.dsy = dsy;
+	}
+
+	public int getV() {
+		return v;
+	}
+
+	public void setV(int v) {
+		this.v = v;
+	}
+
+	public int getTx() {
+		return tx;
+	}
+
+	public void setTx(int tx) {
+		this.tx = tx;
+	}
+
+	public int getTy() {
+		return ty;
+	}
+
+	public void setTy(int ty) {
+		this.ty = ty;
+	}
+
+	public String getImg() {
+		return img;
+	}
+
+	public void setImg(String img) {
+		this.img = img;
+	}
+
+	public Image getImage() {
+		return image;
+	}
+
+	public void setImage(Image image) {
+		this.image = image;
+	}
+
+	public int getAltura() {
+		return altura;
+	}
+
+	public void setAltura(int altura) {
+		this.altura = altura;
+	}
+
+	public int getAmplada() {
+		return amplada;
+	}
+
+	public void setAmplada(int amplada) {
+		this.amplada = amplada;
+	}
+
+	public boolean isDestruit() {
+		return destruit;
+	}
+
+	public void setDestruit(boolean destruit) {
+		this.destruit = destruit;
+	}
+
+	public boolean isPass() {
+		return pass;
+	}
+
+	public void setPass(boolean pass) {
+		this.pass = pass;
+	}
+
+	public Vector<Bala> getListBalas() {
+		return listBalas;
+	}
+
+	public void setListBalas(Vector<Bala> listBalas) {
+		this.listBalas = listBalas;
+	}
+	
+	//Crea el marc de colisio
+	public Rectangle getBounds() {
+		return new Rectangle(x, y, image.getWidth(null)-15,image.getHeight(null)-15);
+	}
+	
 }
 
 //CLASS BALA ================================================================================================================================
 
 class Bala extends Thread {
 	
-	private int posicioVertical;
-	private int posicioHorizontal;
+	private int x;
+	private int y;
 	private int velocidad;
 	private String img;
 	private Image image;
-
-	public Bala() {
-
-	}
-
-	public Bala(int posHorizontal, int posVertical, int velocidad, String direccioImatge) {
-		
-		this.posicioVertical = posVertical;
-		this.posicioHorizontal = posHorizontal;
+	private int altura;
+	private int amplada;
+	private boolean enemic;
+	private boolean destruit;
+	private boolean pass;
+	
+	public Bala(int velocidad, int altura, int amplada, int x, int y, boolean enemic, String direccioImatge) {
 		this.velocidad = velocidad;
+		this.x = x;
+		this.y = y;
+		this.altura = altura;
+		this.amplada = amplada;
+		destruit = false;
+		this.enemic = enemic;
 		image = new ImageIcon(Nau.class.getResource(direccioImatge)).getImage();
 		Thread t = new Thread(this); 
 		t.start();
 	}
 
-	public void moureBala() {
-		posicioVertical -= 6;
-	}
-
 	public void pinta(Graphics g) {
 		Graphics2D g2d = (Graphics2D)g;
-		g2d.drawImage(this.image, posicioHorizontal, posicioVertical, null);
+		g2d.drawImage(this.image, x, y, null);
+	}
+	
+	public void colisio(Graphics g) throws InterruptedException{
+		velocidad=0;
+		destruit = true;
 	}
 
 	public void run() {
-		while (true) { 
-			try { Thread.sleep(this.velocidad); } catch (Exception e) {}
-			moureBala();
+		pass = true;
+		while (pass) {
+			try {
+				Thread.sleep(this.velocidad);
+			} catch (Exception e) {
+			}
+			if (enemic) {
+				moureBalaBaix();
+			} else {
+				moureBalaDalt();
+			}
+		}
+	}
+
+	public void moureBalaBaix (){
+		y = y + velocidad;
+		
+		if(y > altura){
+			destruit = true;
 		}
 	}
 	
-	public int getPosVertical() {
-		return posicioVertical;
+	public void moureBalaDalt (){
+		y = y - velocidad;
+		if( y < 5){
+			destruit = true;
+		}
 	}
 	
-	public int getPosHorizontal() {
-		return posicioHorizontal;
+	//GETTERS AND SETTERS
+	
+	public int getVelocidad() {
+		return velocidad;
+	}
+
+	public void setVelocidad(int velocidad) {
+		this.velocidad = velocidad;
+	}
+	
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public String getImg() {
+		return img;
+	}
+
+	public void setImg(String img) {
+		this.img = img;
+	}
+
+	public Image getImage() {
+		return image;
+	}
+
+	public void setImage(Image image) {
+		this.image = image;
+	}
+
+	public int getAltura() {
+		return altura;
+	}
+
+	public void setAltura(int altura) {
+		this.altura = altura;
+	}
+
+	public int getAmplada() {
+		return amplada;
+	}
+
+	public void setAmplada(int amplada) {
+		this.amplada = amplada;
+	}
+
+	public boolean isEnemic() {
+		return enemic;
+	}
+
+	public void setEnemic(boolean enemic) {
+		this.enemic = enemic;
+	}
+
+	public boolean isDestruit() {
+		return destruit;
+	}
+
+	public void setDestruit(boolean destruit) {
+		this.destruit = destruit;
+	}
+
+	public boolean isPass() {
+		return pass;
+	}
+	//Crea el marc de colisio
+	public Rectangle getBounds() {
+		return new Rectangle(x, y, image.getHeight(null),image.getWidth(null));
+	}
+
+	public void setPass(boolean pass) {
+		this.pass = pass;
 	}
 
 	
+
+
+//=======================================================================================================================
+
+
 }
